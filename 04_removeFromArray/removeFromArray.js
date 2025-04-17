3a3554bf07baa85ab7405cf8e9e43e9e23c2e4c5
HoleInTheUniverse <43988974+HoleInTheUniverse@users.noreply.github.com>
2025-04-17 21:01:05 +0300

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedObjects) {
+    for (let i = 0; i < removedObjects.length; i++) {
+        let index = array.indexOf(removedObjects[i]);
+        
+        while (index !== -1) {
+            array = array.toSpliced(index, 1);
+            index = array.indexOf(removedObjects[i]);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
