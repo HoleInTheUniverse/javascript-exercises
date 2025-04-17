76744d7b118c0a7f64a69a8ca2a50553e05271c0
HoleInTheUniverse <43988974+HoleInTheUniverse@users.noreply.github.com>
2025-04-17 21:32:43 +0300

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if ((x < 1 || y < 1) || (typeof(x) !== "number" || typeof(y) !== "number") || (!Number.isInteger(x) || !Number.isInteger(y))) {
+        return 'ERROR';
+    }
 
+    const nTerm = Math.abs(x - y) + 1;
+    return nTerm / 2 * (x + y);
 };
 
 // Do not edit below this line
